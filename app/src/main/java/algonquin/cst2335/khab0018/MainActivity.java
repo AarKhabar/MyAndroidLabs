d8d427d456288c19e71758e51be4a80e7ac54b98
Aar Khabar <khab0018@algonquinlive.com>
2021-09-22 12:41:04 -0400

    Working on text widgets

--- app/src/main/java/algonquin/cst2335/khab0018/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/khab0018/MainActivity.java
@@ -3,6 +3,10 @@ package algonquin.cst2335.khab0018;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mytext = findViewById(R.id.textview);
+        Button btn = findViewById(R.id.myButton);
+        EditText myedit = findViewById(R.id.editmessage);
+        //String editString = myedit.getText().toString();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String editString =mytext.getText().toString();
+                btn.setOnClickListener(vw -> { mytext.setText("Your edit text has: " + myedit.getText());
+                });
+            }
+        });
+
     }
+
 }

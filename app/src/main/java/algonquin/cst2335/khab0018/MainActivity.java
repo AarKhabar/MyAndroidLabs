424cd358e35995aa5701ac37053da864599f0484
Aar Khabar <khab0018@algonquinlive.com>
2021-09-22 13:39:19 -0400

    Working on compound buttons (Check box,Radio button and Switch)

--- app/src/main/java/algonquin/cst2335/khab0018/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/khab0018/MainActivity.java
@@ -2,11 +2,16 @@ package algonquin.cst2335.khab0018;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
         TextView mytext = findViewById(R.id.textview);
         Button btn = findViewById(R.id.myButton);
         EditText myedit = findViewById(R.id.editmessage);
+        CheckBox checkBox = findViewById(R.id.check);
+        RadioButton radioButton = findViewById(R.id.radio);
+        Switch sw = findViewById(R.id.sw);
         //String editString = myedit.getText().toString();
 
         btn.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +34,34 @@ public class MainActivity extends AppCompatActivity {
                 btn.setOnClickListener(vw -> { mytext.setText("Your edit text has: " + myedit.getText());
                 });
             }
+        });
+
+        sw.setOnCheckedChangeListener( (b, c) -> {
+            Context context = getApplicationContext();
+            CharSequence text = "You clicked on the Switch and it is now:" + c;
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        });
+
+        checkBox.setOnCheckedChangeListener( (b, c) -> {
+            Context context = getApplicationContext();
+            CharSequence text = "You clicked on the Checkbox and it is now:" + c;
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+        });
+
+        radioButton.setOnCheckedChangeListener((b, c) -> {
+            Context context = getApplicationContext();
+            CharSequence text = "You clicked on the Radiobutton and it is now:" + c;
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         });
 
     }

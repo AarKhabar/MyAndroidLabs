04119f79c461a9b08cd46822614c6d6a8ee3c0f2
Aar Khabar <khab0018@algonquinlive.com>
2021-10-24 15:47:29 -0400

    Finish Lab 5

--- app/src/main/java/algonquin/cst2335/khab0018/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/khab0018/ChatRoom.java
@@ -1,15 +1,22 @@
 package algonquin.cst2335.khab0018;
 
 import android.os.Bundle;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -42,7 +49,9 @@ public class ChatRoom extends AppCompatActivity {
 
                 String currentDateandTime = sdf.format(timeNow);
 
-                messages.add(new ChatMessage(whatIsTyped, 1, currentDateandTime));
+                messages.add(new ChatMessage(whatIsTyped,currentDateandTime ,1 ));
+
+
 
                 text.setText("");
 
@@ -57,11 +66,11 @@ public class ChatRoom extends AppCompatActivity {
 
             String currentDateandTime = sdf.format(timeNow);
 
-            messages.add(new ChatMessage(whatIsTyped,  2, currentDateandTime));
+            messages.add(new ChatMessage(whatIsTyped, currentDateandTime ,2));
 
             text.setText("");
 
-            chatAdapter.notifyItemInserted(messages.size() - 2);
+            chatAdapter.notifyItemInserted(messages.size() - 1);
 
             });
         chatAdapter = new MyChatAdapter();
@@ -70,39 +79,65 @@ public class ChatRoom extends AppCompatActivity {
 
     }
 
-        private class MyRoomViews extends RecyclerView.ViewHolder{
+        private class MyRowViews extends RecyclerView.ViewHolder{
             TextView messageText;
             TextView timeText;
-            public MyRoomViews(View itemView) {
+            public MyRowViews(View itemView) {
                 super(itemView);
+                itemView.setOnClickListener(click -> {
+                            int position = getAbsoluteAdapterPosition();
+                            ChatMessage removeMessage = messages.get(position);
+                            MyRowViews newRow = chatAdapter.onCreateViewHolder(null, chatAdapter.getItemViewType(position));
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ChatRoom.this);
+                    builder.setMessage("Do you want to delete this message:" + messageText.getText())
+                            .setTitle("Question")
+                            .setNegativeButton("No", (dialog, cl)->{})
+                            .setPositiveButton("Yes", (dialog, cl)-> {
+                                messages.remove(position);
+                                chatAdapter.notifyItemRemoved(position);
+                                Snackbar.make(messageText, "You deleted message #" + position, Snackbar.LENGTH_LONG)
+                                        .setAction("Undo", clk ->{
+                                         messages.add(position, removeMessage);
+                                         chatAdapter.notifyItemInserted(position);
+                                        }).show();
+
+
+                            }).create().show();
+
+                        });
                 messageText = itemView.findViewById(R.id.message);
                 timeText = itemView.findViewById(R.id.time);
             }
         }
-        private class MyChatAdapter extends RecyclerView.Adapter<MyRoomViews>{
+             private class MyChatAdapter extends RecyclerView.Adapter< MyRowViews > {
 
             @Override
-            public MyRoomViews onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            public MyRowViews onCreateViewHolder(ViewGroup parent, int viewType) {
                 LayoutInflater inflater = getLayoutInflater();
                 int layoutID;
-                if(viewType==1)
+                if (viewType == 1 ) //Send
                     layoutID = R.layout.sent_message;
-                else
+                else //Receive
                     layoutID = R.layout.receive_message;
-                View loadedRow = inflater.inflate(R.layout.sent_message, parent, false);
-                return new MyRoomViews(loadedRow);
+
+                View loadedRow = inflater.inflate(layoutID,parent,false);
+
+                return new MyRowViews(loadedRow);
             }
 
             @Override
             public int getItemViewType(int position) {
-                ChatMessage thisRow = messages.get(position);
+
                 return messages.get(position).getSendOrReceive();
             }
 
             @Override
-            public void onBindViewHolder(MyRoomViews holder, int position) {
-                holder.messageText.setText(messages.get(position).getMessage());
-                holder.timeText.setText(messages.get(position).getTimeSent());
+            public void onBindViewHolder(MyRowViews holder, int position) {
+                ChatMessage thisRow = messages.get(position);;
+                holder.messageText.setText(thisRow.getMessage());
+                holder.timeText.setText(thisRow.getTimeSent());
             }
 
             @Override
@@ -116,7 +151,7 @@ public class ChatRoom extends AppCompatActivity {
         int sendOrReceive;
         String timeSent;
 
-        public ChatMessage(String message, int sendOrReceive, String timeSent) {
+        public ChatMessage(String message, String timeSent,int sendOrReceive) {
             this.message = message;
             this.sendOrReceive = sendOrReceive;
             this.timeSent = timeSent;
